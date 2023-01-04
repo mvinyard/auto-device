32400c15929a45f09dd8a7fe1e53703d899e4028
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-01-04 15:23:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import os
 import sys
 
 setuptools.setup(
-    name="auto-device",
+    name="autodevice",
     version="0.0.1",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
-    url="https://github.com/mvinyard/auto-device",
+    url="https://github.com/mvinyard/autodevice",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    description="",
+    description="Automatically assign available hardware on the fly, in-line with PyTorch code.",
     packages=setuptools.find_packages(),
     install_requires=[
         "torch>=1.12.0",
